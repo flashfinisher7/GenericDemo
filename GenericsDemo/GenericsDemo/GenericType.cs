bc9601015d48eb8aec2d015ee0e8dff54c64cb78
flashfinisher7 <68723204+flashfinisher7@users.noreply.github.com>
2021-08-11 15:32:01 +0530

    Refactor: Generic Demo

---
+++ GenericsDemo/GenericsDemo/GenericType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class GenericType<T>
+    {
+
+            private T[] inputArray;
+
+            public GenericType(T[] inputArray)
+
+            {
+
+                this.inputArray = inputArray;
+
+            }
+
+            public void Toprint()
+
+            {
+
+                foreach (var element in inputArray)
+
+                {
+
+                    Console.WriteLine(element);
+
+                }
+
+                Console.WriteLine("-------------------------------------");
+
+
+
+            }
+
+    }
+}
---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using System.Collections.Generic;
+using GenericsDemo;
+
+namespace Genericarray
+
+{
+    public class program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int[] intArray = { 1, 2, 3, 4 };
+
+            double[] doubleArray = { 1.1, 1.2, 1.3, 1.4};
+
+            char[] charArray = { 'A', 'N', 'I', 'L' };
+
+            new GenericType<int>(intArray).Toprint();
+
+            new GenericType<double>(doubleArray).Toprint();
+
+            new GenericType<char>(charArray).Toprint();
+
+        }
+    }
+}
+
+
